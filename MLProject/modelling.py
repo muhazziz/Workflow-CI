09adc7f2c8f60a0c688aeca85b4b978d944794f0
muhazziz <muhammadazizsyahp@gmail.com>
2026-06-07 15:05:59 +0700

    Fix MLflow run ID conflict

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,9 +5,7 @@ import mlflow
 import mlflow.sklearn
 
 def main():
-    mlflow.set_experiment("Bank_Customer_Churn_CI")
-    mlflow.sklearn.autolog()
-
+    
     print("Memuat dataset...")
     train_df = pd.read_csv("churn_preprocessing/train.csv")
     test_df = pd.read_csv("churn_preprocessing/test.csv")
@@ -15,13 +13,12 @@ def main():
     X_train = train_df.drop('Exited', axis=1)
     y_train = train_df['Exited']
 
-    with mlflow.start_run() as run:
-        print("Melatih model Random Forest...")
-        rf = RandomForestClassifier(random_state=42)
-        rf.fit(X_train, y_train)
-        
-        mlflow.sklearn.log_model(rf, "model")
-        print(f"Run ID: {run.info.run_id}")
+    print("Melatih model Random Forest...")
+    rf = RandomForestClassifier(random_state=42)
+    rf.fit(X_train, y_train)
+    
+    mlflow.sklearn.log_model(rf, "model")
+    print("Model berhasil dilatih dan di-log.")
 
 if __name__ == "__main__":
     main()
