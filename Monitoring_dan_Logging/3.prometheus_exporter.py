ec40169984408006f098d9e915b84fdd01fb4882
muhazziz <muhammadazizsyahp@gmail.com>
2026-06-07 18:46:49 +0700

    fix

--- Monitoring_dan_Logging/3.prometheus_exporter.py
+++ Monitoring_dan_Logging/3.prometheus_exporter.py
@@ -1,11 +1,17 @@
+import joblib
+import pandas as pd
 from prometheus_client import start_http_server, Counter, Gauge, Histogram
 import psutil
 import time
 import random
-import mlflow.pyfunc
-import pandas as pd
 
-model = mlflow.pyfunc.load_model("models:/Bank_Customer_Churn_Prediction/1")
+MODEL_PATH = r"C:\Users\muham_ogp6gj0\Eksperimen_SML_Muh-Azizsyah-Putra\Membangun_model\mlartifacts\1\24a0b28a87024ef8abba8ea2d5062d6e\artifacts\model\model.pkl"
+DATA_PATH = r"C:\Users\muham_ogp6gj0\Eksperimen_SML_Muh-Azizsyah-Putra\Membangun_model\churn_preprocessing\test.csv"
+
+model = joblib.load(MODEL_PATH)
+data = pd.read_csv(DATA_PATH)
+if 'Exited' in data.columns:
+    data = data.drop('Exited', axis=1)
 
 REQUESTS = Counter('http_requests_total', 'Total HTTP Requests')
 LATENCY = Histogram('api_latency_seconds', 'API Latency')
@@ -18,29 +24,17 @@ ACTIVE_CONN = Gauge('active_connections', 'Active Connections')
 DISK_USAGE = Gauge('system_disk_usage', 'Disk Usage Percentage')
 NETWORK_BYTES = Counter('network_bytes_total', 'Network Bytes Transmitted')
 
-def generate_sample():
-    return pd.DataFrame([{
-        "CreditScore": random.randint(300, 850),
-        "Geography": random.choice([0, 1, 2]),
-        "Gender": random.choice([0, 1]),
-        "Age": random.randint(18, 70),
-        "Tenure": random.randint(0, 10),
-        "Balance": round(random.uniform(0, 250000), 2),
-        "NumOfProducts": random.randint(1, 4),
-        "HasCrCard": random.choice([0, 1]),
-        "IsActiveMember": random.choice([0, 1]),
-        "EstimatedSalary": round(random.uniform(10000, 200000), 2)
-    }])
-
 def collect_metrics():
     CPU_USAGE.set(psutil.cpu_percent())
     RAM_USAGE.set(psutil.virtual_memory().percent)
     DISK_USAGE.set(psutil.disk_usage('/').percent)
     REQUESTS.inc(random.randint(1, 5))
+    ACTIVE_CONN.set(random.randint(10, 50))
+    NETWORK_BYTES.inc(random.randint(500, 2000))
 
-    start = time.time()
     try:
-        sample = generate_sample()
+        sample = data.sample(n=1)
+        start = time.time()
         prediction = model.predict(sample)[0]
         LATENCY.observe(time.time() - start)
         if prediction == 1:
@@ -49,9 +43,6 @@ def collect_metrics():
             RETAIN_PREDS.inc()
     except Exception:
         ERRORS.inc()
-
-    ACTIVE_CONN.set(random.randint(10, 50))
-    NETWORK_BYTES.inc(random.randint(500, 2000))
 
 if __name__ == '__main__':
     start_http_server(8000)
